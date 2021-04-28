8a22584f9a59b7a75c4cb70019bab5bf054bc026
henkeattack <59729296+henkeattack@users.noreply.github.com>
2021-04-28 09:54:15 +0200

    ny

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@ export async function getMyInstagramAccMediaStats(instaUserId, accessToken) {
   
   // getMyInstagramAccInsight
   export async function getMyInstagramAccInsight(instaUserId, accessToken) {
-  const url = `https://graph.facebook.com/v8.0/${instaUserId}/insights?metric=impressions,reach&period=week&metric=profile_views&period=day&saccess_token=${accessToken}`;
+  const url = `https://graph.facebook.com/v8.0/${instaUserId}/insights?metric=impressions,reach&period=week&metric=profile_views&period=day&access_token=${accessToken}`;
   let headers = new Headers();
   headers.append("Accept", "application/json");
   return performGetOperation(url, headers);
